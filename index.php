0bddddae9a04eb358e822711a450da8096900db5
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-13 16:14:40 +0200

    Test sur les variables

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
     //Les variables
 
+    $nom = 'BRICE';
+    $note1 = 15;
+    $note2 = 13;
+    $moyenne = ($note1 +$note2) / 2;
+
+    echo "$nom vous avez eu $moyenne de moyenne.\n";
+    echo $nom .' vous avez eu '. $moyenne.' de moyenne.';
 
     //Les tableaux
 
