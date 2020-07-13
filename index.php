c64e44f6fe80d6a4ef1dbb4db69caea04b6a92ca
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-13 16:47:08 +0200

    Test sur les tableaux'

--- index.php
+++ index.php
@@ -11,6 +11,18 @@
 
     //Les tableaux
 
+    $tab1 = [15,16,'Zero','Moins0',20];
+    $tab2 = [
+        'nom' => 'NGABIRANO',
+        'prenom' => 'Brice',
+        'age' => 25,
+        'adresses' => ['kamenge','kinama','mutakura']
+    ];
+
+    echo "\n".$tab1[2];
+    echo "\n".$tab1[4];
+    echo "\n".$tab2['nom'].' '.$tab2['prenom'];
+    echo "\n".$tab2['adresses'][2];
 
     //Les booléens
 
